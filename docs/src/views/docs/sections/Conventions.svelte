b351f29aefd1c22e75cd8a5cd074a7e4ea3d38f6
Leon <leonmichalak6@gmail.com>
2023-04-07 19:12:32 +0100

    (docs) adjusted layouts for few doc sections

--- docs/src/views/docs/sections/Conventions.svelte
+++ docs/src/views/docs/sections/Conventions.svelte
@@ -1,1 +1,17 @@
-<p>Conventions</p>
+<div class="outer-layout">
+    <div class="layout">
+        <h2 style="margin-bottom: 0.1em">Conventions</h2>
+    </div>
+</div>
+
+<style>
+    .outer-layout {
+        padding: 4em 2em;
+    }
+
+    .layout {
+        margin: 0 auto;
+        max-width: 1080px;
+        width: 100%;
+    }
+</style>
--- docs/src/views/docs/sections/Installation.svelte
+++ docs/src/views/docs/sections/Installation.svelte
@@ -1,1 +1,17 @@
-<p>Learn the basics</p>
+<div class="outer-layout">
+    <div class="layout">
+        <h2 style="margin-bottom: 0.1em">Installation</h2>
+    </div>
+</div>
+
+<style>
+    .outer-layout {
+        padding: 4em 2em;
+    }
+
+    .layout {
+        margin: 0 auto;
+        max-width: 1080px;
+        width: 100%;
+    }
+</style>
