a0b6a7effab12b12148c53dd1599dfdf70c71bcc
Leon <leonmichalak6@gmail.com>
2023-04-09 19:00:02 +0100

    (util) add separate entrypoints for browser/server

---
+++ util/src/browser.ts
@@ -0,0 +1,3 @@
+import * as browser from './_browser';
+export * from './_browser';
+export { browser as default };
---
+++ util/src/server.ts
@@ -0,0 +1,3 @@
+import * as server from './_server';
+export * from './_server';
+export { server as default };
