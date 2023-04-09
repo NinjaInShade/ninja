199cb5665153db073ff2a4084d4d957961dd6f5f
Leon <leonmichalak6@gmail.com>
2023-04-09 19:25:16 +0100

    Revert "(util)"
    This reverts commit 029bcb155bd809ecb174a33d80d01de7fca2a963.

--- util/src/shared/isNode.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isNode = void 0;
-var isNode = function () {
-    return typeof process === 'object';
-};
-exports.isNode = isNode;
