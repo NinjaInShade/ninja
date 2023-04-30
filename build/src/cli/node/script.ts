5238bcc97f52b5beec71c372b8256a5a595398e7
Leon <leonmichalak6@gmail.com>
2023-04-30 18:06:19 +0100

    (build) add build support to node script

--- build/src/cli/node/script.ts
+++ build/src/cli/node/script.ts
@@ -1,28 +1,31 @@
 import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { logLine, logInfo, logError } from '../helpers.js';
+import { logLine, logInfo, logError, runAsync } from '../helpers.js';
 import child_process from 'node:child_process';
+import { replaceTscAliasPaths } from 'tsc-alias';
+import fs from 'node:fs/promises';
 
-/**
- * Runtime for node with typescript, compatible with ESM and ts paths
- */
-export async function node(args: Record<string, string>) {
+import fss from 'node:fs';
+import { isAbsolute } from 'node:path';
+
+async function runtime(args) {
     const entryPointFile = args.file;
-    if (!entryPointFile) {
-        logError('file arg is required');
-        return;
-    }
 
+    const cwd = process.cwd();
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
 
-    const cwd = process.cwd();
     const loader = path.join(__dirname, 'loader.js');
     const entryPoint = path.join(cwd, entryPointFile);
 
     // TODO: the idea was to use ts-node-dev however this doesn't work with custom esm loader?? https://github.com/wclr/ts-node-dev/issues/314
-    const nodemon = path.join(__dirname, '../../../node_modules/.bin/nodemon');
-    const subprocess = child_process.spawn(nodemon, ['--exec', 'node', '--experimental-specifier-resolution=node', `--loader=file://${loader}`, entryPoint], { stdio: 'inherit', shell: true });
+    let subprocess: child_process.ChildProcess;
+    if (args['--dev']) {
+        const nodemon = path.join(__dirname, '../../../node_modules/.bin/nodemon');
+        subprocess = child_process.spawn(nodemon, ['--exec', 'node', '--experimental-specifier-resolution=node', `--loader=file://${loader}`, entryPoint], { stdio: 'inherit', shell: true });
+    } else {
+        subprocess = child_process.spawn('node', ['--experimental-specifier-resolution=node', `--loader=file://${loader}`, entryPoint], { stdio: 'inherit', shell: true });
+    }
 
     subprocess.on('error', (err) => {
         logError(`failed to start subprocess: ${err}`);
@@ -41,7 +44,69 @@ export async function node(args: Record<string, string>) {
     });
 }
 
+async function build(args) {
+    const entryPointFile = args.file;
+    const outDir = args.outDir;
+
+    if (!outDir) {
+        logError('outDir arg is required');
+        return;
+    }
+
+    const cwd = process.cwd();
+    const tempFilePath = path.join(cwd, 'node.tsconfig.json');
+    const tempFile = {
+        extends: './tsconfig.json',
+        compilerOptions: {
+            outDir: outDir,
+        },
+        include: [entryPointFile],
+    };
+    await fs.writeFile(tempFilePath, JSON.stringify(tempFile));
+
+    try {
+        try {
+            await runAsync(`tsc --project ${tempFilePath} && tsc-alias --project ${tempFilePath}`);
+        } catch (err) {
+            // pass - most of the time this is just tsc complaining, the command doesn't actually "fail"
+        }
+
+        // TODO?: there is a case this doesn't work, and doesn't replace the alias, if it meets these conditions:
+        // - the file you are aliasing is in the same folder
+        // - you only compile that one folder
+        // Would be nice to get fixed, but it's not so important right now however because:
+        // - if you are importing from same directory, you should just always use relative import "./"
+        // - not very often is the project only going to have one folder to build from
+        await replaceTscAliasPaths({ configFile: tempFilePath, resolveFullPaths: true });
+    } catch (err) {
+        console.log(err);
+        await fs.unlink(tempFilePath);
+    }
+    await fs.unlink(tempFilePath);
+}
+
+/**
+ * Runtime/builder for node with typescript, compatible with ESM and ts paths
+ */
+export async function node(args: Record<string, string>) {
+    const entryPointFile = args.file;
+    if (!entryPointFile) {
+        logError('file arg is required');
+        return;
+    }
+
+    if (args['--build']) {
+        await build(args);
+    } else {
+        await runtime(args);
+    }
+}
+
 export const nodeOptions = {
     // required
-    '(required) file': `The entry point (relative to cwd)`,
+    '(required) file': `The entry point for runtime/building (relative to cwd)`,
+    // optional
+    '(optional) --dev': 'Enables auto reloading when files are changed with nodemon',
+    '(optional) --build': 'Builds the project instead of running node (cwd must have base tsconfig at root)',
+    '(optional) outDir': 'Where to build the project too (relative to cwd, required if --build passed)',
 };
