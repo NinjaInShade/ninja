d6aba06148d63ebda30ef82f2477ff4e4fa301bd
Leon <leonmichalak6@gmail.com>
2023-05-01 14:30:01 +0100

    (build) pass extra args to bin script

--- build/src/cli-index.ts
+++ build/src/cli-index.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 
 import { spawn } from 'child_process';
-spawn('node', ['--loader=./dist/cli/node/loader.js', '--experimental-specifier-resolution=node', './dist/cli/cli.js'], { stdio: 'inherit' });
+const [execPath, jsFile, ...args] = process.argv;
+spawn('node', ['--loader=./dist/cli/node/loader.js', '--experimental-specifier-resolution=node', './dist/cli/cli.js', ...args], { stdio: 'inherit' });
