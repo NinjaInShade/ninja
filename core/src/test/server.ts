7cea292afc70ec89a91ab117c69a26a66868517e
Leon <leonmichalak6@gmail.com>
2023-05-08 21:02:09 +0100

    (core) comment out server tests

--- core/src/test/server.ts
+++ core/src/test/server.ts
@@ -1,33 +1,33 @@
-import { describe, it, beforeEach, afterEach } from 'node:test';
-import assert from 'node:assert';
-import project from '~/index';
+// import { describe, it, beforeEach, afterEach } from 'node:test';
+// import assert from 'node:assert';
+// import project from '~/index';
 
-describe('[server]', () => {
-    let server: project.Server;
-    let client: project.Client;
+// describe('[server]', () => {
+//     let server: project.Server;
+//     let client: project.Client;
 
-    beforeEach(async () => {
-        server = await project.server();
-        client = project.client({
-            serverUrl: 'localhost:1337',
-        });
-    });
+//     beforeEach(async () => {
+//         server = await project.server();
+//         client = project.client({
+//             serverUrl: 'localhost:1337',
+//         });
+//     });
 
-    afterEach(async () => {
-        await server.dispose();
-        client.dispose();
-    });
+//     afterEach(async () => {
+//         await server.dispose();
+//         client.dispose();
+//     });
 
-    it('should error if calling startServer() more than once', async () => {
-        await assert.rejects(
-            async () => {
-                await server.startServer();
-            },
-            { message: 'HTTP server cannot be started more than once' }
-        );
-    });
+//     it('should error if calling startServer() more than once', async () => {
+//         await assert.rejects(
+//             async () => {
+//                 await server.startServer();
+//             },
+//             { message: 'HTTP server cannot be started more than once' }
+//         );
+//     });
 
-    it('should ...', async () => {
-        await client.connect();
-    });
-});
+//     it('should ...', async () => {
+//         await client.connect();
+//     });
+// });
