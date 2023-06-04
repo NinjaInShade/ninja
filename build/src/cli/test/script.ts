17ac326ecfb6b89fc1edd9dfb7ff0f3d90eed553
Leon <leonmichalak6@gmail.com>
2023-06-04 15:33:58 +0100

    (build) updated test script to use new logger

--- build/src/cli/test/script.ts
+++ build/src/cli/test/script.ts
@@ -1,40 +1,36 @@
-import path, { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import child_process from 'node:child_process';
-import { logLine, logInfo, logError, runAsync } from '../helpers';
+import { runAsync, logger } from '@ninjalib/util';
+
+const log = logger('build:test');
 
 /**
  * Test runner
  */
 export async function test(args: Record<string, string>) {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-    const loader = path.join(__dirname, '../', 'node', 'loader.js');
-
     // typical run async util doesn't actually output logs to the console
+    // const __filename = fileURLToPath(import.meta.url);
+    // const __dirname = dirname(__filename);
+    // const loader = path.join(__dirname, '../', 'node', 'loader.js');
     // const subprocess = child_process.spawn('node', ['--experimental-specifier-resolution=node', `--loader=file://${loader}`, '--test-reporter=spec', '--test', './dist/test'], { stdio: 'inherit' });
 
     await runAsync('npm run build');
     const subprocess = child_process.spawn('node', ['--experimental-specifier-resolution=node', `--loader=tsx`, '--test-reporter=spec', '--test', './dist/test'], { stdio: 'inherit' });
 
     subprocess.on('error', (err) => {
-        logError(`failed to start subprocess: ${err}`);
+        log.error(`failed to start subprocess: ${err}`);
     });
 
     subprocess.stdout?.on('data', (data) => {
-        logInfo(`stdout: ${data}`);
+        log.info(`stdout: ${data}`);
     });
 
     subprocess.stderr?.on('data', (data) => {
-        logError(`stderr: ${data}`);
+        log.error(`stderr: ${data}`);
     });
 
     subprocess.on('close', (code) => {
-        logInfo(`child process exited with code ${code}`);
+        log.info(`child process exited with code ${code}`);
     });
 }
 
-export const testOptions = {
-    // required
-    // optional
-};
+export const testOptions = {};
