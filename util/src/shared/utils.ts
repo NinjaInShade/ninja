46aea25426fab520913f95f37975024da1df4eae
Leon <leonmichalak6@gmail.com>
2023-06-09 21:38:46 +0100

    (util) add getEnvVar util & add JSDoc

--- util/src/shared/utils.ts
+++ util/src/shared/utils.ts
@@ -31,14 +31,30 @@ export const colours = {
     bright: {},
 };
 
+/**
+ * @returns true if executing current code in browser
+ */
 export const isBrowser = () => {
     return typeof globalThis.window !== 'undefined';
 };
 
+/**
+ * @returns true if executing current code in node
+ */
 export const isNode = () => {
     return typeof globalThis.process === 'object';
 };
 
+/**
+ * Cross-platform util for finding out if in a production environment
+ */
 export const isProd = (): boolean => {
-    return isBrowser() ? import.meta.env?.PROD ?? false : process.env.NODE_ENV?.trim() === 'production';
+    return isBrowser() ? import.meta?.env?.PROD ?? false : process.env.NODE_ENV === 'production';
+};
+
+/**
+ * Cross-platform util for getting an environment variable
+ */
+export const getEnvVar = (variable: string): string | null => {
+    return (isBrowser() ? import.meta?.env?.[variable] : process.env[variable]) ?? null;
 };
