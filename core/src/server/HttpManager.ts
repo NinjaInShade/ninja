a36acc9a48fa6e540a254fa794a33c1f36a43946
Leon Michalak <leon.michalak@datacap.co.uk>
2023-08-20 09:47:14 +0100

    (core) add CORS accepted headers to middleware

--- core/src/server/HttpManager.ts
+++ core/src/server/HttpManager.ts
@@ -24,6 +24,7 @@ export default class HttpManager {
 
         this._app.use((req, res, next) => {
             res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             next();
         });
     }
