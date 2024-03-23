683cd11fcb42935126e85976a142382afb59f37b
Leon Michalak <leonmichalak6@gmail.com>
2024-03-23 11:06:34 +0000

    (build) use glob pattern for test runner
    the issue with a glob pattern not working when using a loader has since been fixed and we are now using node 21.0.0

--- build/src/cli/test/script.ts
+++ build/src/cli/test/script.ts
@@ -1,7 +1,5 @@
 import child_process from 'node:child_process';
-import path from 'node:path';
 import { logger } from '@ninjalib/util';
-import fsp from 'node:fs/promises';
 
 const log = logger('build:test');
 
@@ -9,17 +7,7 @@ const log = logger('build:test');
  * Test runner
  */
 export async function test({ opts, args }) {
-    // TODO: until https://github.com/nodejs/node/pull/47653 is in node and have updated, test running is restricted to running from test directory directly.
-    // This is due to not being able to give the test runner a directory or glob pattern *when* using a loader - it just finds 0 tests and does nothing.
-
-    const base = path.basename(process.cwd());
-    if (base !== 'test') {
-        log.error('Run tests from the `test` directory');
-        return;
-    }
-
-    const files = (await fsp.readdir('./', { recursive: true })).filter((file) => ['.js', '.ts', '.cjs', '.mjs'].some((ext) => file.endsWith(ext)));
-    const testArgs = ['--experimental-specifier-resolution=node', `--loader=tsx`, '--test-reporter=spec', '--test', ...files];
+    const testArgs = ['--experimental-specifier-resolution=node', `--import=tsx`, '--test-reporter=spec', '--test', '**/test/**/*.{ts,js,mjs}'];
 
     log.debug('Running test runner with:', testArgs);
 
